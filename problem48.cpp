9f88c236e9b39301c57f77d5817bb264576eb08e
giuseros <giuseros85@gmail.com>
2016-12-31 12:30:52 +0000

    problem 49

--- problem48.cpp
+++ problem48.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-#include "bigint/BigIntegerLibrary.hh"
+//#include "bigint/BigIntegerLibrary.hh"
 
 
 using namespace std;
 
 int main(){
-    unsigned long long sol = 0;
-    for (unsigned u = 1; u <= 1000; ++u) {
+    //unsigned long long sol = 0;
+    //for (unsigned u = 1; u <= 1000; ++u) {
 
-	unsigned long long tmp = u;
+    //    unsigned long long tmp = u;
 
-	for (unsigned v = 1; v < u; ++v)
-	  (tmp *= u) %= 10000000000ULL;
+    //    for (unsigned v = 1; v < u; ++v)
+    //      (tmp *= u) %= 10000000000ULL;
 
-	sol += tmp;
+    //    sol += tmp;
 
-    }
-    std::cout << "The last ten digits of the "
-	      << "series, 1^1 + 2^2 + 3^3 + "
-	      << "... + 1000^1000 are: "
-	      << sol % 10000000000ULL <<endl
-	      << sol <<endl;
+    //}
+    //std::cout << "The last ten digits of the "
+    //          << "series, 1^1 + 2^2 + 3^3 + "
+    //          << "... + 1000^1000 are: "
+    //          << sol % 10000000000ULL <<endl
+    //          << sol <<endl;
 
-    return 0;
+    //return 0;
 }
 
--- problem49.cpp
+++ problem49.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <map>
+
 
 using namespace std;
 
 #define N 1e6
 vector<bool> isPrime(N+1,true);
-vector<int> perms;
 
 void sieve()
 {
@@ -19,12 +20,55 @@ void sieve()
     }
 }
 
-void generatePerms()
+vector<int> vectorize(int a){
+	vector<int> digits;
+	while (a){
+		digits.push_back(a%10);
+		a /= 10;
+	}
+	return digits;
+}
+bool verifyPerms(int a, int b, int c)
 {
+	map<int, bool> hist;
+
+	auto digitsA = vectorize(a);
+	auto digitsB = vectorize(b);
+	auto digitsC = vectorize(c);
+
+	if (digitsA.size() != digitsB.size() || digitsA.size() != digitsC.size() ||
+			digitsB.size() != digitsC.size()) return false;
+
+	for (auto d : digitsA){hist[d] = true;}
+	for (auto d : digitsB){if (!hist[d]) return false;}
+	for (auto d : digitsC){if (!hist[d]) return false;}
+
+	return true;
 }
 
 
+bool verify(int a, int b, int c)
+{
+	bool allPrimes = isPrime[a] && isPrime[b] && isPrime[c];
+	bool allPerms = verifyPerms(a,b,c);
+	return allPrimes && allPerms;
+}
+
 int main()
 {
-    return 0;
+	sieve();
+	int x = 1488;
+	int inc = 3330;
+
+	while (true){
+		int a = x;
+		int b = a + inc;
+		int c = b + inc;
+
+		if (verify(a,b,c)){
+			cout<<a<<"-"<<b<<"-"<<c;
+			break;
+		}
+		x++;
+	}
 }
