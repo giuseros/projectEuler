8659d115422a5f065d8013e6932744e972978db7
giuseros <giuseros85@gmail.com>
2017-01-02 11:10:04 +0000

    problem54 - solution

--- problem54.cpp
+++ problem54.cpp
@@ -54,38 +54,45 @@ struct Point
 	PointName name;
 	vector<int> cards;
 	Suit suit;
+	int highestCard;
 
-	Point(PointName n, int v): name(n), cards({v}){}
-	Point(PointName n, Suit s):name(n), suit(s){}
-	Point(PointName n, int v1, int v2): name(n), cards({v1,v2}){}
+	Point(PointName n, int h, int v): name(n), highestCard(h), cards({v}){}
+	Point(PointName n, int h, Suit s):name(n), highestCard(h), suit(s){}
+	Point(PointName n, int h, int v1, int v2): name(n), highestCard(h), cards({v1,v2}){}
 };
 
 bool operator>(Point p1, Point p2)
 {
 	if (p1.name > p2.name) return true;
 	if (p1.name==p2.name){
-
+		if (p1.cards[0] == p2.cards[0]){
+			return p1.highestCard > p2.highestCard;
+		}else{
+			return (p1.cards[0] > p2.cards[0]);
+		}
 	}
 	return false;
 }
 
 bool cardComparator(Card const& c1, Card const& c2)
 {
-	return c1.val < c2.val;
+	return c1.val > c2.val;
 }
 
 bool allCardsConsecutive(vector<Card> hand)
 {
 	for (int i = 0;i<4;i++){
-		if (hand[i].val != hand[i+1].val){
+		if (hand[i].val != hand[i+1].val + 1){
 			return false;
 		}
 	}
 	return true;
 }
 
-Point getPoint(vector<Card> hand){
+Point getPoint(vector<Card> hand, bool debug=0){
 	sort(begin(hand), end(hand), cardComparator);
+
+	auto hCard = hand[0].val;
 	map<int,int> values;
 	map<Suit, int> suits;
 
@@ -97,57 +104,50 @@ Point getPoint(vector<Card> hand){
 	auto numDifferentValues = values.size();
 	auto numDifferentSeeds = suits.size();
 	if (numDifferentSeeds == 1 && hand[0].val == 14 && allCardsConsecutive(hand)){
-
-		for (auto s : suits){ return Point(ROYAL_FLUSH, s.first); }
+		for (auto s : suits){ return Point(ROYAL_FLUSH, hCard, s.first); }
 	} else if (numDifferentSeeds == 1 && allCardsConsecutive(hand)){
-
-		for (auto s : suits){ return Point(STRAIGHT_FLUSH, s.first); }
+		for (auto s : suits){ return Point(STRAIGHT_FLUSH, hCard, s.first); }
 	} else if (numDifferentValues == 2){
 		int cardPair(0), cardTris(0);
 		for (auto v : values){
 			if (v.second == 4){
-				return Point(POKER, v.first);
+				return Point(POKER, hCard, v.first);
 			} else if (v.second == 3){
 				cardTris = v.first;
 			} else {
 				cardPair = v.first;
 			}
 		}
-		return Point(FULL_HOUSE, cardPair, cardTris);
+		return Point(FULL_HOUSE, hCard, cardPair, cardTris);
 	} else if (numDifferentSeeds == 1){
-		for (auto s : suits){ return Point(FLUSH, s.first); }
+		for (auto s : suits){ return Point(FLUSH, hCard, s.first); }
 	} else if (allCardsConsecutive(hand)){
-		return Point(STRAIGHT, hand[0].val);
+		return Point(STRAIGHT, hCard, hand[0].val);
 	} else if (numDifferentValues == 3){
 		int firstPair(0), secondPair(0);
 		for (auto v : values){
 			if (v.second == 3){
-				return Point(TRIS, v.first);
+				return Point(TRIS, hCard, v.first);
 			} else if (v.second==2 && firstPair==0){
 				firstPair = v.first;
 			} else if (v.second ==2){
 				secondPair = v.first;
 			}
 		}
-		return Point(TWO_PAIR, firstPair, secondPair);
+		return Point(TWO_PAIR, hCard, firstPair, secondPair);
 	} else if (numDifferentValues == 4){
-		for (auto v: values){
-			if (v.second ==2){
-				return Point(PAIR, v.first);
-			}
-		}
+		for (auto v: values){ if (v.second ==2) return Point(PAIR, hCard, v.first); }
 	} else {
-		return Point(HIGH_CARD, hand[0].val);
+		return Point(HIGH_CARD, hCard, hand[0].val);
 	}
-
-
 }
 
 bool cmp(vector<Card>  hand1,
-         vector<Card>  hand2)
+         vector<Card>  hand2,
+		 bool debug=0)
 {
-	auto point1 = getPoint(hand1);
-	auto point2 = getPoint(hand2);
+	auto point1 = getPoint(hand1, debug);
+	auto point2 = getPoint(hand2, debug);
 
     return point1 > point2;
 }
@@ -158,21 +158,20 @@ int main()
     fstream f;
 
     // Does not like relative paths
-    f.open("/local/projects/projectEuler/test.txt");
-    for (int i = 0; i < 1;i++){
+    f.open("/local/projects/projectEuler/p054_poker.txt");
+    for (int i = 0; i < 1000;i++){
         string valueAndSuit;
         vector<Card> hand1;
-        for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
             f>>valueAndSuit;
             hand1.emplace_back(valueAndSuit);
         }
 
         vector<Card> hand2;
-        for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
             f>>valueAndSuit;
             hand2.emplace_back(valueAndSuit);
         }
-
         if (cmp(hand1, hand2)){
             count++;
         }
