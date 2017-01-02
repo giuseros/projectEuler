a3d117f964b64a2babc8402bb979e2f3f4fa7f83
giuseros <giuseros85@gmail.com>
2017-01-02 23:14:40 +0000

    problem 55 skeleton

--- problem55.cpp
+++ problem55.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int reverseAndAdd(int n)
+{
+	return 0;
+}
+
+bool isPalindrome(int n){
+	return true;
+}
 
 int main()
 {
-
+	int count(0);
+	for (int i = 0;i<=10000;i++){
+		int n = i;
+		for (int k = 0;i<50;k++){
+			n = reverseAndAdd(n);
+			if (isPalindrome(n)){
+				count++;
+				break;
+			}
+		}
+	}
+	cout<<count<<endl;
 }
