47bc1fc556b0b13d04a02e1d33df3c8363e9d9bb
giuseros <giuseros85@gmail.com>
2017-01-03 14:36:45 +0100

    problem 55

--- problem55.cpp
+++ problem55.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
-
-int reverseAndAdd(int n)
+using long_t = unsigned long long;
+long_t reverse(long_t n)
 {
-	return 0;
+	long_t t(n), r(0);
+//	cout<<n<<endl;
+	while(t){
+		r = r*10 + t%10;
+		t /= 10;
+	}
+//	cout<<r<<endl;
+	return r;
 }
 
-bool isPalindrome(int n){
-	return true;
+long_t reverseAndAdd(long_t n)
+{
+	return n+reverse(n);
+}
+
+bool isPalindrome(long_t n){
+	return reverse(n) == n;
 }
 
 int main()
 {
 	int count(0);
-	for (int i = 0;i<=10000;i++){
-		int n = i;
-		for (int k = 0;i<50;k++){
+
+	for (int i = 196;i<1e4;i++){
+		long_t n = i;
+		bool isLychrel(true);
+
+		for (int k = 0;k<50;k++){
 			n = reverseAndAdd(n);
 			if (isPalindrome(n)){
-				count++;
+				isLychrel = false;
 				break;
 			}
+		}
+		if (isLychrel){
+			count++;
 		}
 	}
 	cout<<count<<endl;
